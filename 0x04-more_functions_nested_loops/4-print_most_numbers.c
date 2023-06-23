5a541611b9d1151971dfbc2b60cf5c9fa140d5a8
rod160691 <oseikwasirod@gmail.com>
2023-06-23 16:36:37 -0700

    print 0 - 9 without 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,10 +9,10 @@ void print_most_numbers(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		if (num != 2 && num != 4)
+		if (num != 2 && != 4)
 		{
 			_putchar(num + '0');
 		}
-		putchar('\n');
-	}	
+	}
+	_putchar('\n');
 }
